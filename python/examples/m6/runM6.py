cf88968290f04a82a6262514b1950f3a7264e138
kenway <kenway@localhost>
2013-04-24 12:04:06 -0400

    added several examples. BWB, m6, 717, and sphere cases work. CRM case does not. 2D examples are currently broken. Also fixed output mirror bug

---
+++ python/examples/m6/runM6.py
@@ -0,0 +1,49 @@
+import sys, os, time
+from mdo_import_helper import import_modules, MPI, mpiPrint
+sys.path.append('../../')
+import pyHyp
+import petsc4py
+petsc4py.init(sys.argv)
+from petsc4py import PETSc
+
+options= {
+    # ---------------------------
+    #        Grid Parameters
+    # ---------------------------
+    'N': 65,
+    's0':1.5e-6,
+    'rMin':60,
+
+    # ---------------------------
+    #   Pseudo Grid Parameters
+    # ---------------------------
+    'NMax':1000,
+    'ps0':1.5e-6,
+    'pGridRatio':1.125,
+    'cMax':6,
+
+    # ---------------------------
+    #   Smoothing parameters
+    # ---------------------------
+    'epsE': 1.0,
+    'epsI': 2.0,
+    'theta': 3.0,
+    'volCoef': .16,
+    'volBlend': 0.0005,
+    'volSmoothIter': 10,
+    
+    # ---------------------------
+    #   Solution Parameters
+    # ---------------------------
+    'kspRelTol': 1e-5,
+    'kspMaxIts': 500,
+    'preConLag': 5,
+    'kspSubspaceSize':50,
+    }
+
+hyp = pyHyp.pyHyp('3d',fileName='m6_small.fmt', options=options, zMirror=True)
+#hyp = pyHyp.pyHyp('3d',fileName='m6_large.fmt', options=options, zMirror=True)
+
+hyp.run()
+hyp.writeCGNS('m6.cgns')
+#hyp.writeCGNSOrig('m6Orig.cgns')
