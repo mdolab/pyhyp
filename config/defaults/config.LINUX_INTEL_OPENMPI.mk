99f3f93d7611ffa1e470180c0ce15943e8607235
kenway <kenway@localhost>
2015-11-10 18:08:26 -0500

    Update to default configs

--- config/defaults/config.LINUX_INTEL_OPENMPI.mk
+++ config/defaults/config.LINUX_INTEL_OPENMPI.mk
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------
-# Config file for Intel ifort  with OpenMPI_
+# Config file for Intel ifort  with OpenMPI
 # ----------------------------------------------------------------------
 
 # ------- Define a possible parallel make ------------------------------
@@ -10,22 +10,29 @@ FF90 = mpif90
 CC   = mpicc
 
 # ------- Define CGNS Inlcude and linker flags -------------------------
-CGNS_INCLUDE_FLAGS = -I/usr/local/include
-CGNS_LINKER_FLAGS = -Wl,-rpath,/usr/local/lib -lcgns
+CGNS_INCLUDE_FLAGS=-I$(HOME)/packages/cgnslib_3.2.1/src
+CGNS_LINKER_FLAGS=-L$(HOME)/packages/cgnslib_3.2.1/src -lcgns
 
 # ------- Define Compiler Flags ----------------------------------------
-FF90_GEN_FLAGS = 
-CC_GEN_FLAGS   =
+FF90_GEN_FLAGS = -fPIC
+CC_GEN_FLAGS   = -fPIC
 
-FF90_OPT_FLAGS   =  -fPIC -r8 -O2  
-CC_OPT_FLAGS     = -O2 -fPIC
+FF90_OPT_FLAGS   =  -fPIC -r8 -O2 
+CC_OPT_FLAGS     = -O2 
 
 # ------- Define Linker Flags ------------------------------------------
 LINKER_FLAGS = -nofor_main
 
 # ------- Define Petsc Info --- Should not need to modify this -----
-include ${PETSC_DIR}/conf/variables
+include ${PETSC_DIR}/lib/petsc/conf/variables # PETSc 3.6
+#include ${PETSC_DIR}/conf/variables # PETSc 3.5
 PETSC_INCLUDE_FLAGS=${PETSC_CC_INCLUDES} -I$(PETSC_DIR)
 PETSC_LINKER_FLAGS=${PETSC_LIB}
+
+# Define potentially different python, python-config and f2py executables:
+PYTHON = python
+PYTHON-CONFIG = python-config
+F2PY = f2py
+
 
 
--- config/defaults/config.LINUX_INTEL_OPENMPI_SCINET.mk
+++ config/defaults/config.LINUX_INTEL_OPENMPI_SCINET.mk
@@ -10,22 +10,26 @@ FF90 = mpif90
 CC   = mpicc
 
 # ------- Define CGNS Inlcude and linker flags -------------------------
-CGNS_INCLUDE_FLAGS = -I$(HOME)/../kenway/packages/cgnslib_2.5
-CGNS_LINKER_FLAGS  = -Wl,-rpath,$(HOME)/../kenway/packages/cgnslib_2.5/LINUX -L$(HOME)/../kenway/packages/cgnslib_2.5/LINUX -lcgns
+CGNS_INCLUDE_FLAGS=-I$(HOME)/packages/cgnslib_3.2.1/src
+CGNS_LINKER_FLAGS=-L$(HOME)/packages/cgnslib_3.2.1/src -lcgns
 
 # ------- Define Compiler Flags ----------------------------------------
-FF90_GEN_FLAGS = 
-CC_GEN_FLAGS   =
+FF90_GEN_FLAGS = -fPIC
+CC_GEN_FLAGS   = -fPIC
 
-FF90_OPT_FLAGS   =  -fPIC -r8 -O2
-CC_OPT_FLAGS     = -O2 -fPIC
+FF90_OPT_FLAGS   =  -fPIC -r8 -O2 
+CC_OPT_FLAGS     = -O2 
 
 # ------- Define Linker Flags ------------------------------------------
 LINKER_FLAGS = -nofor_main
 
 # ------- Define Petsc Info --- Should not need to modify this -----
-include ${PETSC_DIR}/conf/variables
+include ${PETSC_DIR}/lib/petsc/conf/variables # PETSc 3.6
+#include ${PETSC_DIR}/conf/variables # PETSc 3.5
 PETSC_INCLUDE_FLAGS=${PETSC_CC_INCLUDES} -I$(PETSC_DIR)
 PETSC_LINKER_FLAGS=${PETSC_LIB}
 
-
+# Define potentially different python, python-config and f2py executables:
+PYTHON = python
+PYTHON-CONFIG = python-config
+F2PY = f2py
