03653d6ba38c557c4754becd7bdec7fb312957e9
Gaetan Kenway <gaetank@gmail.com>
2018-01-19 15:02:36 -0800

    updated pyhyp to use module-based cgns3.3

--- Hyp_Common.mk
+++ Hyp_Common.mk
@@ -29,7 +29,7 @@ endif
 #      *                                                                *
 #      ******************************************************************
 
-.SUFFIXES: .o .f .F .f90 .F90 
+.SUFFIXES: .o .f .F .f90 .F90
 
 #      ******************************************************************
 #      *                                                                *
@@ -48,8 +48,8 @@ MAKE_CLEAN_ARGUMENTS = *~ *.o *.mod *.il *.stb c_*
 #      *                                                                *
 #      ******************************************************************
 
-FF90_ALL_FLAGS   = -I$(HYP_MODDIR) $(CGNS_INCLUDE_FLAGS) \
-		   $(FF90_GEN_FLAGS) $(FF90_OPT_FLAGS) $(PETSC_INCLUDE_FLAGS) 
+FF90_ALL_FLAGS   = -I$(HYP_MODDIR) $(CGNS_INCLUDE_FLAGS) $(CGNS_VERSION_FLAG) \
+		   $(FF90_GEN_FLAGS) $(FF90_OPT_FLAGS) $(PETSC_INCLUDE_FLAGS)
 
 CC_ALL_FLAGS = -I$(HYP_MODDIR) $(CGNS_INCLUDE_FLAGS) \
 		   $(CC_GEN_FLAGS) $(CC_OPT_FLAGS) $(PETSC_INCLUDE_FLAGS)
