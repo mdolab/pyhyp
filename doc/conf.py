c40b5ee56d3f5f370b8119899a678b73f1004b77
Neil Wu <neilwu@umich.edu>
2020-10-14 16:10:08 -0400

    updated conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -1,4 +1,12 @@
-from sphinx_mdolab_theme.config import copyright, exclude_patterns, master_doc, html_static_path, html_context
+from sphinx_mdolab_theme.config import (
+    copyright,
+    exclude_patterns,
+    master_doc,
+    html_static_path,
+    html_context,
+    html_theme,
+    extensions,
+)
 
 # -- Path setup --------------------------------------------------------------
 
@@ -15,21 +23,10 @@ sys.path.insert(0, os.path.abspath("../"))
 project = "pyHyp"
 
 # -- General configuration -----------------------------------------------------
-# Add any Sphinx extension module names here, as strings. They can be extensions
-# coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.todo",
-    "sphinx.ext.coverage",
-    "sphinx.ext.imgmath",
-    "sphinx.ext.viewcode",
-    "numpydoc",
-]
+# Built-in Sphinx extensions are already contained in the imported variable
+# here we add external extensions, which must also be added to requirements.txt
+# so that RTD can import and use them
+extensions.extend(["numpydoc"])
 
 # mock import for autodoc
 autodoc_mock_imports = ["numpy", "mpi4py"]
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = "sphinx_mdolab_theme"
