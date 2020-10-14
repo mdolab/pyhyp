a07f110e81386324f8b3efa5d4e246d13a4aa884
Neil Wu <neilwu@umich.edu>
2020-10-14 16:35:48 -0400

    Triggering new build

--- doc/conf.py
+++ doc/conf.py
@@ -9,7 +9,6 @@ from sphinx_mdolab_theme.config import (
 )
 
 # -- Path setup --------------------------------------------------------------
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
