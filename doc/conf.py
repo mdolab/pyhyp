914f9dd4e2aee4d0f6b8e70ecc67080e7751488b
Neil Wu <neilwu@umich.edu>
2020-10-18 10:57:54 -0400

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -1,13 +1,4 @@
-from sphinx_mdolab_theme.config import (
-    copyright,
-    exclude_patterns,
-    master_doc,
-    html_static_path,
-    html_context,
-    html_theme,
-    html_theme_options,
-    extensions,
-)
+from sphinx_mdolab_theme.config import *
 
 # -- Path setup --------------------------------------------------------------
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -27,6 +18,7 @@ project = "pyHyp"
 # here we add external extensions, which must also be added to requirements.txt
 # so that RTD can import and use them
 extensions.extend(["numpydoc"])
+numpydoc_show_class_members = False
 
 # mock import for autodoc
 autodoc_mock_imports = ["numpy", "mpi4py"]
