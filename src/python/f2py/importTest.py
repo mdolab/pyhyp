0c10638e82590e3cefd33075fec2bfcc5ffbbf6a
Marco Mangano <mmangano@umich.edu>
2020-10-30 17:10:40 +0100

    completed black formatting

--- src/python/f2py/importTest.py
+++ src/python/f2py/importTest.py
@@ -1,15 +1,14 @@
 #! /usr/bin/env python
 import sys
 
-name = 'hyp'
-print ("Testing if module %s can be imported..." % name)
+name = "hyp"
+print("Testing if module %s can be imported..." % name)
 import_cmd = "import %s" % name
 try:
     exec(import_cmd)
 except:
-    print ("Error: hyp.so was not imported correctly")
+    print("Error: hyp.so was not imported correctly")
     sys.exit(1)
 # end try
 
 print("Module %s was successfully imported." % name)
-
