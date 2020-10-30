5946beca735e56f1877ccffca333a9ff676605d3
Marco Mangano <mmangano@umich.edu>
2020-10-30 13:12:59 +0100

    reformatted MExt + some questions to be answered

--- pyhyp/MExt.py
+++ pyhyp/MExt.py
@@ -1,4 +1,10 @@
-import tempfile, imp, os, shutil, sys
+import tempfile
+import imp
+import os
+import shutil
+import sys
+
+
 def _tmp_pkg(dir):
     """
     Create a temporary package.
@@ -12,11 +18,12 @@ def _tmp_pkg(dir):
             modinfo = imp.find_module(name)
             # if name is found, delete and try again
             os.rmdir(path)
-        except:
+        except:  # What kind of exception is this?
             break
-    init = open(os.path.join(path, '__init__.py'), 'w')
+    init = open(os.path.join(path, "__init__.py"), "w")
     init.close()
     return name, path
+
 
 class MExt(object):
     """
@@ -38,7 +45,7 @@ class MExt(object):
         # import the module
         # __import__ returns the package, not the sub-module
         self._pkg = __import__(self._pkgname, globals(), locals(), [self.name])
-        # remove the bogus directory from sys.path 
+        # remove the bogus directory from sys.path
         sys.path.remove(tmpdir)
         # return the module object
         self._module = getattr(self._pkg, self.name)
@@ -53,7 +60,7 @@ class MExt(object):
             # on win32, the DLL must be unloaded forcefully in order to delete it.
             # on Darwin (other unix???) this doesn't appear to be necessary
             # try to unload the dll
-            if os.name == 'nt':
+            if os.name == "nt":  # Are we supporting other OS ???
                 hModule = win32api.GetModuleHandle(self._module.__file__)
                 win32api.FreeLibrary(hModule)
             # now try to delete the files and directory
@@ -65,5 +72,5 @@ class MExt(object):
             # and the exception is caught here
             try:
                 __import__(self.name)
-            except:
+            except:  # What kind of exception is this?
                 pass
