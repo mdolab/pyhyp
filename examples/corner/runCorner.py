856afedec916682dd98132557cdc07f752631643
Marco Mangano <36549388+marcomangano@users.noreply.github.com>
2020-11-03 20:17:58 +0100

    Format update and improved exception - output handling (#31)

--- examples/corner/runCorner.py
+++ examples/corner/runCorner.py
@@ -1,63 +1,57 @@
-from pyhyp import pyHyp, pyHypMulti
+from pyhyp import pyHypMulti
 
-'''
+"""
 In this example we extrude a 90 deg corner twice using two different
 setting.
 This example shows how the dictionary format can be used to set up
 and run multiple cases at once
-'''
+"""
 
-fileName = 'corner.cgns'
-fileType = 'CGNS'
+fileName = "corner.cgns"
+fileType = "CGNS"
 
-commonOptions= {
-
+commonOptions = {
     # ---------------------------
     #        Input Parameters
     # ---------------------------
-    'inputFile':fileName,
-    'fileType': 'CGNS',
-    'unattachedEdgesAreSymmetry':False,
-    'outerFaceBC':'farfield',
-    'autoConnect':True,
-    'BC':{},
-    'families':'wall',
-
+    "inputFile": fileName,
+    "fileType": "CGNS",
+    "unattachedEdgesAreSymmetry": False,
+    "outerFaceBC": "farfield",
+    "autoConnect": True,
+    "BC": {},
+    "families": "wall",
     # ---------------------------
     #        Grid Parameters
     # ---------------------------
-    'N': 65, 
-    's0': 1e-6,
-    'marchDist':2.5,
-    'splay':0.5,
+    "N": 65,
+    "s0": 1e-6,
+    "marchDist": 2.5,
+    "splay": 0.5,
     # ---------------------------
     #   Pseudo Grid Parameters
     # ---------------------------
-    'ps0': -1,
-    'pGridRatio': -1,
-    'cMax': 5,
-
+    "ps0": -1,
+    "pGridRatio": -1,
+    "cMax": 5,
     # ---------------------------
     #   Smoothing parameters
     # ---------------------------
-    'epsE': 1.0,
-    'epsI': 2.0,
-    'theta': 3.0,
-    'volCoef': 0.25,
-    'volBlend': 0.0001,
-    'volSmoothIter': 100,
+    "epsE": 1.0,
+    "epsI": 2.0,
+    "theta": 3.0,
+    "volCoef": 0.25,
+    "volBlend": 0.0001,
+    "volSmoothIter": 100,
 }
 
 # Now set up specific options
-options1 = {'outputFile':'corner1_hyp.cgns'}
+options1 = {"outputFile": "corner1_hyp.cgns"}
 
-options2 = {'epsE':4.0,
-            'epsI':8.0,
-            'outputFile':'corner2_hyp.cgns'}
+options2 = {"epsE": 4.0, "epsI": 8.0, "outputFile": "corner2_hyp.cgns"}
 
 # Gather options in a list
-options = [options1,
-           options2]
+options = [options1, options2]
 
-hyp = pyHypMulti(options=options,commonOptions=commonOptions)
+hyp = pyHypMulti(options=options, commonOptions=commonOptions)
 hyp.combineCGNS()
--- pyhyp/__init__.py
+++ pyhyp/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '2.2.0'
+__version__ = "2.2.0"
 
 from .pyHyp import pyHyp, pyHypMulti
