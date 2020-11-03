d1c5a8ca6e82ba80be54446645e4eceff157007a
Marco Mangano <mmangano@umich.edu>
2020-11-03 19:03:57 +0100

    gotcha!

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,7 @@ project = "pyHyp"
 # Built-in Sphinx extensions are already contained in the imported variable
 # here we add external extensions, which must also be added to requirements.txt
 # so that RTD can import and use them
-extensions.extend(["numpydoc"])  # noqa: F405
+extensions.extend(["numpydoc"])
 numpydoc_show_class_members = False
 
 # mock import for autodoc
