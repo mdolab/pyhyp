2194bdd0fc76aa1f0344530f1011c20fda1231f9
Eirikur Jonsson <eirikurj@umich.edu>
2021-02-26 21:45:34 +0000

    Removing CGNS support for versions older than 3.3.0

--- src/modules/cgnsGrid.F90
+++ src/modules/cgnsGrid.F90
@@ -1,14 +1,7 @@
 module cgnsGrid
 
   use precision
-#ifdef USECGNSMODULE
   use cgns
   implicit none
-#else
-  implicit none
-  include "cgnslib_f.h"
-  integer(kind=4), private :: dummyInt
-  integer, parameter :: cgsize_t=kind(dummyInt)
-#endif
 
 end module cgnsGrid
