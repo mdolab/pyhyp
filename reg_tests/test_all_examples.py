f4df38b439e596bb911d4fa6b29ebba9afe18204
sseraj <sseraj@umich.edu>
2021-08-20 14:17:00 -0400

    changed examples directory import

--- reg_tests/test_all_examples.py
+++ reg_tests/test_all_examples.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 import unittest
 
 baseDir = os.path.dirname(os.path.abspath(__file__))
@@ -9,6 +10,10 @@ refDir = os.path.join(baseDir, "ref")
 class TestExamples(unittest.TestCase):
 
     N_PROCS = 2
+
+    def setUp(self):
+        # Insert the repo root in path so that testflo can import examples
+        sys.path.insert(0, os.path.join(baseDir, "../"))
 
     def common_test(self, testFile, marchDist, relTol=1e-14):
         volumeName = os.path.split(testFile)[1]
@@ -28,49 +33,49 @@ class TestExamples(unittest.TestCase):
             self.assertEqual(output, "/CGNSLibraryVersion <> /CGNSLibraryVersion : data values differ")
 
     def test_2D_euler(self):
-        from pyhyp_examples.naca0012.naca0012_euler import volumeFile, hyp
+        from examples.naca0012.naca0012_euler import volumeFile, hyp
 
         marchDist = hyp.getOption("marchDist")
         self.common_test(volumeFile, marchDist)
 
     def test_2D_rans(self):
-        from pyhyp_examples.naca0012.naca0012_rans import volumeFile, hyp
+        from examples.naca0012.naca0012_rans import volumeFile, hyp
 
         marchDist = hyp.getOption("marchDist")
         self.common_test(volumeFile, marchDist)
 
     def test_717(self):
-        from pyhyp_examples.wing_717.run717 import volumeFile, hyp
+        from examples.wing_717.run717 import volumeFile, hyp
 
         marchDist = hyp.getOption("marchDist")
         self.common_test(volumeFile, marchDist)
 
     def test_BWB(self):
-        from pyhyp_examples.BWB.runBWB import volumeFile, hyp
+        from examples.BWB.runBWB import volumeFile, hyp
 
         marchDist = hyp.getOption("marchDist")
         self.common_test(volumeFile, marchDist)
 
     def test_corner(self):
-        from pyhyp_examples.corner.runCorner import volumeFile, commonOptions
+        from examples.corner.runCorner import volumeFile, commonOptions
 
         marchDist = commonOptions["marchDist"]
         self.common_test(volumeFile, marchDist)
 
     def test_M6(self):
-        from pyhyp_examples.m6.runM6 import volumeFile, hyp
+        from examples.m6.runM6 import volumeFile, hyp
 
         marchDist = hyp.getOption("marchDist")
         self.common_test(volumeFile, marchDist)
 
     def test_plate(self):
-        from pyhyp_examples.plate.runPlate import volumeFile, hyp
+        from examples.plate.runPlate import volumeFile, hyp
 
         marchDist = hyp.getOption("marchDist")
         self.common_test(volumeFile, marchDist)
 
     def test_sphere(self):
-        from pyhyp_examples.sphere.runSphere import volumeFile, commonOptions
+        from examples.sphere.runSphere import volumeFile, commonOptions
 
         marchDist = commonOptions["marchDist"]
         self.common_test(volumeFile, marchDist)
