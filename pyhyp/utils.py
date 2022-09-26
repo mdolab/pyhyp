262099a281c3b0db3d06cf85f706eb1dd4cd2027
Anil Yildirim <42349285+anilyil@users.noreply.github.com>
2022-09-26 19:48:33 -0400

    Added options to pass grid object, bounds, outer BC to simpleOCart (#66)
    * modifications to simple o cart to handle custom bounding box and overset BC on the outer face
    
    * modifications to simpleocart
    
    * addressing comments

--- pyhyp/utils.py
+++ pyhyp/utils.py
@@ -2,18 +2,21 @@ import os
 import tempfile
 import numpy as np
 from mpi4py import MPI
-from cgnsutilities.cgnsutilities import readGrid, Block
+from baseclasses.utils import Error
+from cgnsutilities.cgnsutilities import readGrid, Block, simpleCart, Grid
 from .pyHyp import pyHyp
 
 
-def simpleOCart(nearFile, dh, hExtra, nExtra, sym, mgcycle, outFile, userOptions=None):
+def simpleOCart(inputGrid, dh, hExtra, nExtra, sym, mgcycle, outFile, userOptions=None, xBounds=None, useFarfield=True):
     """
     Generates a Cartesian mesh around the provided grid, surrounded by an O-mesh.
 
     Parameters
     ----------
-    nearFile : str
-        The name of the nearfield CGNS file to mesh around.
+    inputGrid : cgnsutils Grid object or str
+        If a cgnsutils Grid object is provided, we use it as is.
+        Alternatively if a string is provided, we treat it as
+        the name of the nearfield CGNS file to mesh around.
 
     dh : float or list of float
         The target edge length of each cell in the Cartesian part of the mesh.
@@ -38,15 +41,45 @@ def simpleOCart(nearFile, dh, hExtra, nExtra, sym, mgcycle, outFile, userOptions
         Output file name.
 
     userOptions : dict, optional
-        The name of the nearfield CGNS file to mesh around.
+        Custom pyhyp options to be used with this extrusion. If overset BCs are desired
+        on the outer face, do not set it in this dictionary because after extrusion
+        we overwrite all BCs on the combined grid. See the option useFarfield
+        below. The default value (True) results in farfield BCs on the outer face,
+        and setting it to false results in overset for the far face. Other pyhyp extrusion
+        parameters can be set here.
+
+    xBounds : array (2 x 3), optional
+        Optional bounding box coordinates desired for the center cartesian grid.
+        The default value can be obtained by: ``xMin, xMax = grid.getBoundingBox()``,
+        and then the xBounds array can be set as ``xBounds = [xMin, xMax]``. This option
+        allows users to modify the bounding box coordinates rather than simply defaulting
+        to the bounding box of the nearfield grid.
+
+    useFarfield : bool, optional
+        Optional flag to control the outermost layer's BC. Default, ``True``, will result
+        in a farfield outer layer, setting this to ``False`` does overset BCs on the
+        outermost layer
 
     """
 
-    # Read the nearfield file
-    grid = readGrid(nearFile)
+    # check if we have a grid object as input or the filename
+    if type(inputGrid) == str:
+        # Read the nearfield file
+        input_filename = inputGrid
+        inputGrid = readGrid(input_filename)
+    # if the input grid is not provided as a filename, it must be a Grid instance
+    elif type(inputGrid) != Grid:
+        # if not, raise an error
+        raise Error(
+            "The inputGrid to simpleOCart must either be the filename of the nearfield grid or a Grid type object from cgnsutilities."
+        )
 
-    # First run simpleCart with no extension
-    X, dx = grid.simpleCart(dh, 0.0, 0, sym, mgcycle, outFile=None)
+    if xBounds is None:
+        # we will automatically determine the bounding box
+        X, dx = inputGrid.simpleCart(dh, 0.0, 0, sym, mgcycle, outFile=None)
+    else:
+        # we are provided the bounding box, skip to the generic simple cart routine
+        X, dx = simpleCart(xBounds[0], xBounds[1], dh, 0, 0, sym, mgcycle, outFile=None)
 
     # Pull out the patches from the Cartesian mesh.
     # We have to pay attention to the symmetry and the ordering of the patches
@@ -73,7 +106,6 @@ def simpleOCart(nearFile, dh, hExtra, nExtra, sym, mgcycle, outFile, userOptions
     hypOptions = {
         "patches": patches,
         "unattachedEdgesAreSymmetry": True,
-        "outerFaceBC": "farfield",
         "autoConnect": True,
         "BC": {},
         "N": nExtra,
@@ -107,14 +139,17 @@ def simpleOCart(nearFile, dh, hExtra, nExtra, sym, mgcycle, outFile, userOptions
 
     if MPI.COMM_WORLD.rank == 0:
         # Read the pyhyp mesh back in and add our additional "X" from above.
-        grid = readGrid(fName)
+        simple_ocart_grid = readGrid(fName)
         dims = X.shape[0:3]
-        grid.addBlock(Block("interiorBlock", dims, X))
-        grid.renameBlocks()
-        grid.connect()
-        grid.BCs = []
-        grid.autoFarfieldBC(sym)
-        grid.writeToCGNS(outFile)
+        simple_ocart_grid.addBlock(Block("interiorBlock", dims, X))
+        simple_ocart_grid.renameBlocks()
+        simple_ocart_grid.connect()
+        simple_ocart_grid.BCs = []
+        if useFarfield:
+            simple_ocart_grid.autoFarfieldBC(sym)
+        else:
+            simple_ocart_grid.autoNearfieldBC(sym)
+        simple_ocart_grid.writeToCGNS(outFile)
 
         # Delete the temp file
         os.remove(fName)
