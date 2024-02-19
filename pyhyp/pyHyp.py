7ac86d7f1c1251850a3cbaddfdaf7feb03ad2f16
Hannah Hajdik <hajdik@umich.edu>
2024-02-18 19:19:38 -0600

    allow for inputfile or outputfile for case name in printout

--- pyhyp/pyHyp.py
+++ pyhyp/pyHyp.py
@@ -212,7 +212,14 @@ class pyHypMulti(object):
             elif type(options) is dict:
                 # Only the root processor will print
                 if myid == 0:
-                    print("\n\nRunning case %d : %s\n" % (index, options["outputFile"]))
+                    # change the printout based on which option is actually present
+                    if "inputFile" in options.keys():
+                        caseName = options["inputFile"]
+                    elif "outputFile" in options.keys():
+                        caseName = options["outputFile"]
+                    else:
+                        caseName = "case"
+                    print("\n\nRunning case %d : %s\n" % (index, caseName))
 
                 # Create pyHyp object using the corresponding options
                 hypGrid = pyHyp(comm, options, debug)
