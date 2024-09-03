4df92b464a6e5049af23d4ac67365f94a4194491
andv <andv@zhaw.ch>
2024-09-03 14:08:55 +0200

    Add missing test "n0012 schedule"

---
+++ examples/naca0012/naca0012_schedule.py
@@ -0,0 +1,88 @@
+"""
+This script uses the NACA 0012 airfoil equation to generate a 2D RANS mesh.
+This mesh has a blunt trailing edge. 
+
+This script is the same as 'naca0012_rans.py' except for some 'schedule'
+parameters that were changed.
+"""
+import os
+import numpy
+from pyhyp import pyHyp
+
+baseDir = os.path.dirname(os.path.abspath(__file__))
+surfaceFile = os.path.join(baseDir, "naca0012_rans.fmt")
+volumeFile = os.path.join(baseDir, "naca0012_schedule.cgns")
+
+alpha = numpy.linspace(0, 2 * numpy.pi, 273)
+x = numpy.cos(alpha) * 0.5 + 0.5
+y = numpy.zeros_like(x)
+
+for i in range(len(x)):
+    if i < len(x) / 2:
+        y[i] = 0.6 * (
+            0.2969 * numpy.sqrt(x[i]) - 0.1260 * x[i] - 0.3516 * x[i] ** 2 + 0.2843 * x[i] ** 3 - 0.1015 * x[i] ** 4
+        )
+    else:
+        y[i] = -0.6 * (
+            0.2969 * numpy.sqrt(x[i]) - 0.1260 * x[i] - 0.3516 * x[i] ** 2 + 0.2843 * x[i] ** 3 - 0.1015 * x[i] ** 4
+        )
+
+# Since the TE is open we need to close it. Close it multiple linear segments.
+delta_y = numpy.linspace(y[-1], y[0], 32, endpoint=True)
+delta_y = delta_y[1:]
+
+x = numpy.append(x, numpy.ones_like(delta_y))
+y = numpy.append(y, delta_y)
+
+# Write the plot3d input file:
+f = open(surfaceFile, "w")
+f.write("1\n")
+f.write("%d %d %d\n" % (len(x), 2, 1))
+for iDim in range(3):
+    for j in range(2):
+        for i in range(len(x)):
+            if iDim == 0:
+                f.write("%g\n" % x[i])
+            elif iDim == 1:
+                f.write("%g\n" % y[i])
+            else:
+                f.write("%g\n" % (float(j)))
+f.close()
+
+options = {
+    # ---------------------------
+    #        Input Parameters
+    # ---------------------------
+    "inputFile": surfaceFile,
+    "unattachedEdgesAreSymmetry": False,
+    "outerFaceBC": "farfield",
+    "autoConnect": True,
+    "BC": {1: {"jLow": "zSymm", "jHigh": "zSymm"}},
+    "families": "wall",
+    # ---------------------------
+    #        Grid Parameters
+    # ---------------------------
+    "N": 129,
+    "s0": 1e-6,
+    "marchDist": 100.0,
+    # ---------------------------
+    #   Pseudo Grid Parameters
+    # ---------------------------
+    "ps0": -1.0,
+    "pGridRatio": -1.0,
+    "cMax": 3.0,
+    # ---------------------------
+    #   Smoothing parameters
+    # ---------------------------
+    "epsESchedule": [[0.0,1.0], [1.0, 5.0]],
+    "epsISchedule": [[0.0, 2.0], [1.0, 10.0]],
+    "thetaSchedule": [[0.0, 3.0],[1.0, 0.0]],
+    "volCoef": 0.25,
+    "volBlendSchedule": [[0.0, 0.0001, 1.0, 0.1]],
+    "volSmoothSchedule": [[0.0, 100], [1.0, 500]],
+}
+
+
+hyp = pyHyp(options=options)
+hyp.run()
+hyp.writeCGNS(volumeFile)
